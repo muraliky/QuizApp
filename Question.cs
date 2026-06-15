e9546b7d7bc2c65ac6954085ba128c470d830749
muraliky <76985483+muraliky@users.noreply.github.com>
2026-06-15 14:28:39 +0530

    Delete Question.cs

--- Question.cs
+++
@@ -1,9 +0,0 @@
-namespace QuizApp
-{
-    public class Question
-    {
-        public string QuestionText { get; set; }
-        public string[] Options { get; set; }
-        public int CorrectAnswerIndex { get; set; }
-    }
-}
